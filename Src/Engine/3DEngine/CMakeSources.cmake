a5653738b9b3fc2f5d266e3460bf0079a5e6287d
Kevo Macartney <kevomacartney@hotmail.com>
2020-05-02 22:51:03 +0100

    removed MoltenVk because we're using find_package now

---
+++ Src/Engine/3DEngine/CMakeSources.cmake
@@ -0,0 +1,13 @@
+
+set(VULKAN_INC # include directories
+        "vulkanUtility.h"
+        )
+
+set(VULKAN_SRC # source directories
+        "vulkanUtility.cpp"
+        )
+
+set(VULKAN_PLUGIN_SRC
+        ${VULKAN_INC}
+        ${VULKAN_SRC}
+        )
